3c1af0bd63807458a687ee39ae9b7fa0a3a19d18
KevinMayfield <mayfield.g.kev@gmail.com>
2019-05-18 13:19:50 +0100

    Sample BPMN deployment

--- src/main/java/uk/gov/dwp/workflow/dao/TaskDao.java
+++ src/main/java/uk/gov/dwp/workflow/dao/TaskDao.java
@@ -3,10 +3,14 @@ package uk.gov.dwp.workflow.dao;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.engine.repository.Model;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.gov.dwp.workflow.providers.ConformanceProvider;
 
 import java.util.List;
 
@@ -19,6 +23,13 @@ public class TaskDao implements ITask {
     @Autowired
     private RuntimeService runtimeService;
 
+    @Autowired
+    private RepositoryService repositoryService;
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TaskDao.class);
+
+
+
     @Override
     public Task
     read(FhirContext ctx, IdType internalId) {
@@ -28,12 +39,21 @@ public class TaskDao implements ITask {
     @Override
     public Task create(FhirContext ctx, Task task) {
 
-        runtimeService.startProcessInstanceByKey("oneTaskProcess");
+        runtimeService.startProcessInstanceByKey("ESA1");
         return task;
     }
 
     @Override
     public List<Task> search(FhirContext ctx, ReferenceParam patient) throws Exception {
+        Model model = repositoryService.getModel("123");
+
+        List<Deployment> deployments = repositoryService.createDeploymentQuery().list();
+
+        for (Deployment deployment : deployments) {
+            List<String> deploymentResources = repositoryService.getDeploymentResourceNames(deployment.getId());
+
+            log.info(deploymentResources.toString());
+        }
 
         taskService.createTaskQuery().taskAssignee("NHS-9876543210").list();
         return null;
